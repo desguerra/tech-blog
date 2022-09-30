e15d635f214e342e08f2eef0fa277814f21fe75b
desguerra <ddesguerra@ucdavis.edu>
2022-09-29 17:12:19 -0700

    populate posts from db

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,49 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-        id: 1,
-        title: 'Handlebars Docs',
-        content: 'hello, hello world!!!',
-        created_at: new Date(),
-        comments: [{}, {}],
-        user: {
-            username: 'test_user',
-        },
-    });
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_text',
+                    'post_id',
+                    'user_id',
+                    'created_at',
+                ],
+                include: {
+                    model: User,
+                    attributes: ['username'],
+                },
+            },
+            {
+                model: User,
+                attributes: ['username'],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            // pass a single post object into the homepage template
+            const posts = dbPostData.map((post) => post.get({ plain: true }));
+            // pass a single post object into the homepage template
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
