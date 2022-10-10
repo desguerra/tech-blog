def813cd03d24a521755d3b23ea454bed401b8c9
desguerra <ddesguerra@ucdavis.edu>
2022-10-10 14:20:35 -0700

    refactor messy code and add README

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# tech-blog
+# Module #14 Challenge: The Tech Blog
+
+## Description
+
+This project is a **full-stack blog site** where developers can publish their blog posts and comment on other developers' post. This site was built completely from scratch and is deployed on Heroku. This application follows the **Model-View-Controller (MVC)** architectural structure, utilizes **Handlebar.js** as the template engine, **Sequelize** as the ORM, and the **express-session** npm package for authentication.
+
+[Check out the deployed application here!]()
+
+## Table of Contents
+
+* [Screenshots](#screenshots)
+* [Questions](#questions)
+
+
+## Screenshots
+
+![screenshot 1 of project](assets/SS1.PNG)
+
+![screenshot 2 of project](assets/SS2.PNG)
+
+![screenshot 3 of project](assets/SS3.PNG)
+
+![screenshot 4 of project](assets/SS4.PNG)
+
+![screenshot 5 of project](assets/SS5.PNG)
+
+
+## Questions
+
+If you have any questions about this repository, please open an issue. You can also find more of my work at [desguerra](https://github.com/desguerra) on GitHub.
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,11 +1,11 @@
-async function loginFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
-
-    const username = document.querySelector('#user-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
+  
+    const username = document.querySelector('#user-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
@@ -14,12 +14,14 @@ async function loginFormHandler(event) {
             headers: { 'Content-Type': 'application/json' },
         });
 
+        // check the response status
         if (response.ok) {
             document.location.replace('/');
+            console.log('success');
         } else {
             alert(response.statusText);
         }
     }
 }
 
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
